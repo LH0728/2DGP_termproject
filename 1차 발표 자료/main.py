d342b9cff7924a3758176a4d2e4fee39dcdcdf4b
Lee Hyeong uk <dlguddnr1018@tukorea.ac.kr>
2025-10-15 22:10:03 +0900

    pico2d 창 열기

--- 1차 발표 자료/main.py
+++ 1차 발표 자료/main.py
@@ -1,6 +1,21 @@
 from pico2d import *
 
-open_canvas(800, 600)
+def handle_events():
+    global running
 
-while True :
-    pass
+    event_list = get_events()
+    for event in event_list:
+        if event.type == SDL_QUIT:
+            running = False
+        elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
+            running = False
+
+
+open_canvas(1200, 800)
+running = True
+
+while running :
+    handle_events()
+    delay(0.01)
+
+close_canvas()
