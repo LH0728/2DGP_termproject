82d49ff4c7e916068e56e8b30514e653a33100c2
Lee Hyeong uk <dlguddnr1018@tukorea.ac.kr>
2025-11-12 17:46:16 +0900

    흙 이미지 크기 변경

--- mine_2.py
+++ mine_2.py
@@ -6,8 +6,8 @@ class Mine_2:
         self.image = load_image('bg4_finish.png')
         # Soil 객체를 담을 리스트 생성
         self.soils = []
-        # Mining_1_1.png 이미지 크기를 49x49로 변경
-        soil_width, soil_height = 48, 49
+        # Soil 객체의 생성 간격을 실제 크기인 60x60으로 수정합니다.
+        soil_width, soil_height = 60, 60
         # y좌표 0부터 150까지, x좌표 0부터 1200까지 Soil 객체로 채우기
         for y in range(0, 150, soil_height):
             for x in range(0, 1200, soil_width):
@@ -33,9 +33,14 @@ class Soil:
         self.x, self.y = x, y
 
     def draw(self):
-        # 이미지의 왼쪽 위 (0, 0) 위치에서 49x49 크기로 잘라내어 그립니다.
-        # clip_draw의 y좌표는 이미지의 하단을 기준으로 하므로, 이미지 높이에서 49를 빼줍니다.
-        self.image.clip_draw(0, self.image.h - 49, 49, 49, self.x, self.y, 60, 60)
+
+        self.image.clip_draw(3, self.image.h - 45, 42, 42, self.x, self.y, 60, 60)
+        draw_rectangle(*self.get_bb())
 
     def update(self, character):
         pass
+
+    def get_bb(self):
+        # 객체의 중심 좌표(self.x, self.y)와 크기(60x60)를 기반으로
+        # 왼쪽, 아래, 오른쪽, 위쪽 좌표를 반환합니다.
+        return self.x - 30, self.y - 30, self.x + 30, self.y + 30
